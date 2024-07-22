b4e7fa65d237aca9753f48b35abfc184baa2c803
Михаил <misha13022008@gmail.com>
2024-07-22 12:36:17 +0300

    logger added

---
+++ Wist/Logger/FileLogger.cs
@@ -0,0 +1,22 @@
+﻿namespace Wist.Logger;
+
+public class FileLogger : ILogger
+{
+    public FileLogger()
+    {
+        File.WriteAllText("logs.txt", "");
+    }
+
+    public void Log(string msg, string filePath = "", int lineNumber = 0)
+    {
+        File.AppendAllText("logs.txt", MakeLogMessage());
+
+        string MakeLogMessage()
+        {
+            var prefix = $"path: '{filePath}'; line: {lineNumber}; ";
+            if (msg.Contains('\n'))
+                return prefix + $"\n{msg}";
+            return prefix + msg;
+        }
+    }
+}
---
+++ Wist/Logger/ILogger.cs
@@ -0,0 +1,8 @@
+﻿namespace Wist.Logger;
+
+using System.Runtime.CompilerServices;
+
+public interface ILogger
+{
+    public void Log(string msg, [CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = 0);
+}
